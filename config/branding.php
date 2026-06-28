6003ec6c5119adc68649c2832298a0eb349814dc
Marcus Povey <marcus@instruct-eric.eu>
2026-06-28 14:23:36 +0100

    feat: add branding logo and update logo URL in configuration

--- config/branding.php
+++ config/branding.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'logo_url' => env('BRAND_LOGO_URL', ''),
+    'logo_url' => env('BRAND_LOGO_URL') ?: '/images/meetquorum-logo.svg',
     'banner_url' => env('BRAND_BANNER_URL', ''),
     'primary_color' => env('BRAND_PRIMARY_COLOR', '#4F46E5'),
     'favicon_url' => env('BRAND_FAVICON_URL', ''),
